8a952c3a27a459ca057226b9a56c8908442ad20b
Sven van Duijn <sven@vanduijn.net>
2019-01-03 11:33:11 +0100

    Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { ProductpageComponent } from './productpage/productpage.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: '', component: ProductpageComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignupComponent,
+    LoginComponent,
+    ProductpageComponent,
+    CartComponent,
+    CheckoutComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes, {enableTracing: true}
+    ),
     BrowserModule
   ],
   providers: [],
